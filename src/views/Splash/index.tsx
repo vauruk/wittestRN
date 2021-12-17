e2a06e8f9aefed92a112a4b97bd189dee4909b03
Vanderson Vauruk <vauruk@gmail.com>
2021-12-17 09:11:55 -0300

    retirado comentarios

--- src/views/Splash/index.tsx
+++ src/views/Splash/index.tsx
@@ -1,23 +1,8 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 
-import {
-    Container,
-    Content,
-    Label,
-    Title,
-    ColA,
-    ColB,
-    Row,
-    LabelLink,
-    ActivityIndicatorVK,
-} from './styles';
-import { ImageVK } from '../../components';
-import { Linking } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { CityRoutes } from '../../routes/City/types';
 import { Props } from './types';
-import { IconVK } from '../common/styles';
-import vanderson from '../../assets/vanderson.png';
 import LottieView from 'lottie-react-native';
 
 const Splash: React.FC<Props> = (props: Props) => {
@@ -27,39 +12,7 @@ const Splash: React.FC<Props> = (props: Props) => {
             navigation.navigate(CityRoutes.CityList);
         }, 5000);
     });
-    const gitHub: string = 'https://github.com/vauruk';
-    const handlePress = useCallback(async () => {
-        await Linking.openURL(gitHub);
-    }, [gitHub]);
-
     return <LottieView source={require('./lottie2.json')} autoPlay loop />;
-    // <Container>
-    //     <Content>
-    //         <ImageVK source={vanderson} width={250} height={300} />
-    //         <Title>Vanderson de Moura Vauruk</Title>
-    //         <Row>
-    //             <ColA flex={0.2}>
-    //                 <IconVK name="at" size={30} />
-    //             </ColA>
-    //             <ColB flex={0.8}>
-    //                 <Label>vauruk@gmail.com</Label>
-    //             </ColB>
-    //         </Row>
-    //         <Row>
-    //             <ColA flex={0.2}>
-    //                 <IconVK name="github" size={30} />
-    //             </ColA>
-    //             <ColB flex={0.8}>
-    //                 <LabelLink onPress={() => handlePress()}>
-    //                     {gitHub}
-    //                 </LabelLink>
-    //             </ColB>
-    //         </Row>
-    //         <ActivityIndicatorVK size="large" />
-
-    //     </Content>
-    // </Container>
-    //);
 };
 
 export default Splash;
